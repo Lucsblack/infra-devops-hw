9803bd0bbe9e93a261de07acb7473e92c896162d
lucsblack <lucasregog3@gmail.com>
2025-05-31 21:18:44 -0300

    feat: finalize deploy script and nginx config

--- app/server.py
+++ app/server.py
@@ -1,17 +1,12 @@
 from flask import Flask
-import socket
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    hostname = socket.gethostname()
-    return f"""
-    <h1>H&W Publishing</h1>
-    <p>Server: {hostname}</p>
-    <p>Port: {5000 if '5000' in hostname else 5001}</p>
-    <style>body {{ font-family: Arial; text-align: center; margin-top: 50px; }}</style>
-    """
+def hello():
+    return f'Hello World from port {os.environ.get("PORT", 5000)}!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
